067c6efc93ccc1ff04f888372a57634df610586f
khbyun <novemberde@nate.com>
2018-03-25 11:16:57 +0900

    set default user_id

--- serverless-api/app.js
+++ serverless-api/app.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// 실제로 사용한다고 가정하면 유저정보를 실어주어야함.
+app.use((req, res, next) => {
+    res.locals.user_id = "1";
+    next();
+});
+
 app.get("/", (req, res, next) => {
     res.send("hello world!");
 });
