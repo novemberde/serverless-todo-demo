4913aa5a393d4df673f1875c0e6c9938dd57510d
khbyun <novemberde@nate.com>
2018-06-03 23:27:20 +0900

    Modify user_id to userId

--- serverless-api/routes/todo.js
+++ serverless-api/routes/todo.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const dynamoose = require('dynamoose');
 const _ = require('lodash');
 const Todo = dynamoose.model('Todo', {
-    user_id: {
+    userId: {
         type: String,
         hashKey: true
     }, 
@@ -14,14 +14,14 @@ const Todo = dynamoose.model('Todo', {
     title: String,
     content: String
 }, {
-    create: false, // Create table in DB, if it does not exist,
+    create: false, // Create a table if not exist,
 });
 
 router.get("/", (req, res, next) => {
-    const user_id = res.locals.user_id;
+    const userId = res.locals.userId;
     let lastKey = req.query.lastKey;
     
-    return Todo.query('user_id').eq(user_id).startAt(lastKey).limit(10).exec((err, result) => {
+    return Todo.query('userId').eq(userId).startAt(lastKey).limit(10).exec((err, result) => {
         if(err) return next(err, req, res, next);
         
         res.status(200).json(result);
@@ -29,10 +29,10 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:createdAt", (req, res, next) => {
-    const user_id = res.locals.user_id;
+    const userId = res.locals.userId;
     const createdAt = String(req.params.createdAt);
 
-    return Todo.get({user_id, createdAt}, function (err, result) {
+    return Todo.get({userId, createdAt}, function (err, result) {
         if(err) return next(err, req, res, next);
       
         res.status(200).json(result);
@@ -40,12 +40,12 @@ router.get("/:createdAt", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-    const user_id = res.locals.user_id;
+    const userId = res.locals.userId;
     const body = req.body;
     
     body.createdAt = new Date().toISOString();
     body.updatedAt = new Date().toISOString();
-    body.user_id = user_id;
+    body.userId = userId;
     
     return new Todo(body).save((err, result) => {
         if(err) return next(err, req, res, next);
@@ -55,7 +55,7 @@ router.post("/", (req, res, next) => {
 });
 
 router.put("/:createdAt", (req, res, next) => {
-    const user_id = res.locals.user_id;
+    const userId = res.locals.userId;
     const createdAt = req.params.createdAt;
     const body = req.body;
     
@@ -64,7 +64,7 @@ router.put("/:createdAt", (req, res, next) => {
     body.updatedAt = new Date().toISOString(); 
     
     return new Todo(_.assign(body, {
-        user_id,
+        userId,
         createdAt
     })).save((err, result) => {
         if(err) return next(err, req, res, next);
@@ -75,12 +75,12 @@ router.put("/:createdAt", (req, res, next) => {
 
 router.delete("/:createdAt", (req, res, next) => {
     const createdAt = req.params.createdAt;
-    const user_id = res.locals.user_id;
+    const userId = res.locals.userId;
     
     if(!createdAt) return res.status(400).send("Bad request. createdAt is undefined");
     
     return Todo.delete({
-        user_id,
+        userId,
         createdAt
     }, (err) => {
         if(err) return next(err, req, res, next);
