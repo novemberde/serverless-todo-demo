731140abe9219ae3d6aed3e3e44f352a853472f4
khbyun <novemberde@nate.com>
2018-06-19 19:15:27 +0900

    On testing cors

--- serverless-api/app.js
+++ serverless-api/app.js
@@ -5,29 +5,34 @@ const app = express();
 
 require("aws-sdk").config.region = "ap-northeast-2"
 
-app.use(cors());
+// app.use(cors());
+app.all('*', (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // 실제로 사용한다고 가정하면 유저정보를 실어주어야함.
 app.use((req, res, next) => {
-    res.locals.userId = "1";
-    next();
+	res.locals.userId = "1";
+	next();
 });
 
 app.get("/", (req, res, next) => {
-    res.send("hello world!");
+	res.send("hello world!");
 });
 
 app.use("/todo", require("./routes/todo"));
 
 app.use((req, res, next) => {
-    res.status(404).send("Not Found");
+	return res.status(404).send("Not Found");
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err);
+	console.error(err);
+	return res.status(500).send(err);
 });
 
 module.exports = app;
